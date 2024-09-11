2f7b25b790792ee05308661fb3a0b9069b77ef86
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-09-11 11:12:53 +0900

    styled-components

--- react-study202409/react-study/src/components/CourseGoals/CourseInput.js
+++ react-study202409/react-study/src/components/CourseGoals/CourseInput.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
 import './CourseInput.css';
-import Button from '../UI/Button';
+import ButtonComponent from '../UI/Button';
 
 const CourseInput = ({ onAdd }) => {
   const [enteredText, setEnteredText] = useState('');
 
+  // 입력값 검증용 상태 변수
+  const [isValid, setIsValid] = useState(true);
+
   const textChangeHnadler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredText(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault(); // form 기능 중지
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     onAdd(enteredText); // App.js에게 입력값 넘기기
     setEnteredText(''); // 입력창 비우기
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
         <input type='text' value={enteredText} onChange={textChangeHnadler} />
       </div>
-      <Button type='submit'>목표 추가하기</Button>
+      <ButtonComponent type='submit'>목표 추가하기</ButtonComponent>
     </form>
   );
 };
--- react-study202409/react-study/src/components/UI/Button.js
+++ react-study202409/react-study/src/components/UI/Button.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import './Button.css';
-// 반복되는 태그가 다양한 속성을 가지게 된다면
-// 컴포넌트로 분할해서 속성값을 일괄적으로 처리할 수 있게 하자.
-const Button = ({ type, onClick, children }) => {
+import styled from 'styled-components';
+// import './Button.css';
+
+// styled-components가 제공하는 styled를 사용해서 원하는 태그를 생성하고
+// 해당 태그에 디자인을 문자열로 바로 지정할 수 있습니다.
+
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+
+`;
+// styled-components를 사용하면서 props를 처리하고 싶다면
+// 중간 단계의 컴포넌트를 하나 더 춛가하면 됩니다.
+const ButtonComponent = ({ type, onClick, children }) => {
   return (
-    <button type={type} onClick={onClick}>
+    <Button type={type} onClick={onClick}>
       {children}
-    </button>
+    </Button>
   );
 };
 
-export default Button;
+// 반복되는 태그가 다양한 속성을 가지게 된다면
+// 컴포넌트로 분할해서 속성값을 일괄적으로 처리할 수 있게 하자.
+// const Button = ({ type, onClick, children }) => {
+//   return (
+//     <button className='button' type={type} onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// };
+
+export default ButtonComponent;
