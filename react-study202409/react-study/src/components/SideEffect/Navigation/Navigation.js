14a3a150a613e9f8c2bb06c39d655a33877cbbbd
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-09-13 11:22:50 +0900

    useContext를 활용한 context Store

--- react-study202409/react-study/src/components/SideEffect/Navigation/Navigation.js
+++ react-study202409/react-study/src/components/SideEffect/Navigation/Navigation.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
 import AuthContext from '../Store/auth-context';
 
-const Navigation = ({ onLogout }) => {
+const Navigation = () => {
+  // Context store에 좀 더 쉽게 접근해서 소비할 수 있게 해주는 useConstext hook.
+  // AuthContext 에서 제공하는 전역 객체를 디스트럭처링으로 분해.
+
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            {context.isLoggedIn && (
-              <ul>
-                <li>
-                  <a href='#'>MyPage</a>
-                </li>
-                <li>
-                  <a href='#'>Admin</a>
-                </li>
-                <li>
-                  <button onClick={() => onLogout()}>Logout</button>
-                </li>
-              </ul>
-            )}
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      {isLoggedIn && (
+        <ul>
+          <li>
+            <a href='#'>MyPage</a>
+          </li>
+          <li>
+            <a href='#'>Admin</a>
+          </li>
+          <li>
+            <button onClick={() => onLogout()}>Logout</button>
+          </li>
+        </ul>
+      )}
+    </nav>
   );
 };
 
